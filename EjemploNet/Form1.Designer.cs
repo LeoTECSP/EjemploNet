08de35c8d560494dcaae10c6593aa7a8356532c4
ZR <101909289+LeoTECSP@users.noreply.github.com>
2023-02-23 12:11:05 -0600

    primera version

---
+++ EjemploNet/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EjemploNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCheckConnection = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(480, 361);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnCheckConnection
+            // 
+            this.btnCheckConnection.Location = new System.Drawing.Point(602, 38);
+            this.btnCheckConnection.Name = "btnCheckConnection";
+            this.btnCheckConnection.Size = new System.Drawing.Size(136, 23);
+            this.btnCheckConnection.TabIndex = 1;
+            this.btnCheckConnection.Text = "Revisar Conexión";
+            this.btnCheckConnection.UseVisualStyleBackColor = true;
+            this.btnCheckConnection.Click += new System.EventHandler(this.btnCheckConnection_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheckConnection);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCheckConnection;
+    }
+}
+
---
+++ EjemploNet/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckConnection_Click(object sender, EventArgs e)
+        {
+            //acá ya trabajamos con ADO.NET, no usamos la palabra driver
+            string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=AdventureWorks2019;Integrated Security=True";
+            //el . es para representar una instancia local
+            //servidor, base de datos, usuario y contraseña, pero como no usamos con contraseña se usa la de windows
+
+            //Crea una conexión a la base de datos
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                //Abrimos la conexión a base de datos
+                connection.Open();
+
+                //Si la conexión se realizó correctamente, muestra un mensaje al usuario
+                MessageBox.Show("Conexión exitosa a la base de datos","Conexión exitosa",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                //Si la conexión no se pudo realizar, muestra un mensaje al usuario con el error ocurrido
+                MessageBox.Show("Error: {0}", ex.Message, MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+                throw;
+            }
+            finally 
+            {
+            //Cierra la conexión a la base de datos
+            connection.Close(); 
+            }
+        }
+    }
+}
